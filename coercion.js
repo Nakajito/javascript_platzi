bf4cb5a09a2b35855b8f0efcd451b88fca7474f6
Daniel Bonilla <87622543+Nakajito@users.noreply.github.com>
2023-02-16 19:01:59 +0000

    clase 10 coercion

---
+++ coercion.js
@@ -0,0 +1,11 @@
+/*
+coerciones implicitas: js nos ayuda a cambiar el tipo de valor.
+coerciones explicitas: obligamos a que cambie el tipo de valor.
+*/
+
+//coercion implicita
+let a = 4 + "7";
+let b = 4 * "7";
+
+//coercion explicita
+let c = String(b);
